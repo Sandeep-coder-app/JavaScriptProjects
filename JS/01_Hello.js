1cb1eede99af3062d132c9b16f767675fd2669b2
Sandeep-coder-app <appdevsandeep@gmail.com>
2023-12-08 10:33:36 +0530

    js

--- JS/01_Hello.js
+++ JS/01_Hello.js
@@ -3,3 +3,5 @@ console.log("Sandeep Singh")
 console.log("Add",3+5) // Add 8
 
 console.log("Add "+3+5) // Add 35
+
+console.log(35-3*2+5); //34
---
+++ JS/print_name.js
@@ -0,0 +1,6 @@
+name = "Sandeep Singh"
+
+for(i = 0; i <10; i++)
+{
+    console.log(name)
+}
