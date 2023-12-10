b9d1a6fab32214e3255c1a58810f4bf720778b80
Sandeep-coder-app <appdevsandeep@gmail.com>
2023-12-10 22:55:36 +0530

    odd or even

---
+++ JS/04_var.js
@@ -0,0 +1,6 @@
+/* 
+javascripts var statement is used to declare variable in javascript that are function-scoped.
+The var statement is also used to decaler global-scoped variables.
+
+Before the intro of ES6 all the keywords in javascripts
+*/
