b390c3987693fd807abbd6d432aee08868ec1924
Sandeep-coder-app <appdevsandeep@gmail.com>
2023-12-23 14:23:20 +0530

    del

--- DSA/DS_ARRAY/01_prog.js
+++
@@ -1,9 +0,0 @@
-let arr = [0,1,2,3,4,5]
-const len = arr.length
-console.log(len);
-
-const arr1 = ['Sandeep Singh','Rohit Yadav','Akash Yadav']
-console.table(arr1)
-
-const arr2 = ['9','2','1','8','10','6']
-console.table(arr2.sort())
