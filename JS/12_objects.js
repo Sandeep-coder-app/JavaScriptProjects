8a8872e01601c8e8fbc039903b3310b4c6510deb
Sandeep-coder-app <appdevsandeep@gmail.com>
2024-01-23 22:08:38 +0530

    12_objects

--- JS/12_objects.js
+++ JS/12_objects.js
@@ -29,5 +29,9 @@ const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "c", 4: "d"}
 
 // const obj3 = {obj1, obj2}
-const obj3 = Object.assign({}, obj1, obj2)
-console.log(obj3);
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+console.log(Object.keys(obj1));
