c546317ef72c6cd073f12f33877725e9f99decf2
Sandeep-coder-app <appdevsandeep@gmail.com>
2024-02-06 18:50:52 +0530

    loop

--- JS/17_loop.js
+++ JS/17_loop.js
@@ -92,5 +92,36 @@ const myCoding = [
 ]
 
 myCoding.forEach( (item) => {
-    console.log(item.languageName);
+    // console.log(item.languageName);
 })
+
+
+const coding = ['js', 'java', 'python', 'cpp1']
+
+// const values = coding.forEach( (item) =>  {
+//    // console.log(item);
+//    return item;
+// })
+
+// console.log(values);
+
+//
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter( (item) => item > 4)
+
+
+// const newNums = myNums.filter( (item) => {
+//     return item > 4
+// })
+
+
+const newNums = []
+
+myNums.forEach( (num) => {
+    if (num > 4) {
+        newNums.push(num)
+    }
+})
+
+console.log(newNums);
