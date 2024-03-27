c553ebb9e89e9b0e05477511f8b847a8b34c09d3
Sandeep-coder-app <appdevsandeep@gmail.com>
2024-03-27 22:02:00 +0530

    present the element

---
+++ DSA/Array/03_array.js
@@ -0,0 +1,11 @@
+const arr = [10, 5, 6, 7, 99]
+
+const x = 10
+
+for(let i = 0; i < arr.length; i++) {
+    if (arr[i] == x) {
+        console.log("Present the element");
+    } else {
+        console.log("Not present the element");
+    }
+}
